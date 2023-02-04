7fdfc8f210edf6a33cc43cfb51ae4aa3155346e5
Solastis <60881212+Solastis@users.noreply.github.com>
2023-02-04 14:12:14 +0100

    V 0.1.0 - 2023-02-04
    Seperated commands into seperate files
    Added logging for injection
    Added chrono crate for date/time

---
+++ src-tauri/src/commands/inject_command.rs
@@ -0,0 +1,42 @@
+use chrono;
+use dll_syringe::process::OwnedProcess;
+use dll_syringe::Syringe;
+use std::fs::File;
+use std::io::prelude::*;
+
+/// Injects a dll into a process
+/// # Arguments
+/// * `process` - The name of the process to inject into
+/// * `dll_path` - The path to the dll to inject
+#[tauri::command]
+pub fn run_command(process: &str, dll_path: &str) {
+    // TODO : Make this not depend on dll_syringe
+
+    // Find the process that the user wants to inject into
+    let target_process = OwnedProcess::find_first_by_name(&process).unwrap();
+
+    // Creates Syringe instance with the target process
+    // Injects the dll into the process
+    let instance = Syringe::for_process(target_process);
+
+    let result = instance.inject(dll_path).unwrap();
+
+    // Create folder if it doesn't exist in the running directory with the name sola
+    std::fs::create_dir_all("sola").unwrap();
+    let mut file = File::create(format!(
+        "sola\\log-{}.txt",
+        chrono::Local::now().format("%Y-%m-%d %H-%M-%S")
+    ))
+    .unwrap();
+
+    // Write the header to the file
+    file.write_all(format!("Process: {}\r", process).as_bytes())
+        .unwrap();
+    file.write_all(format!("DLL: {}\r", dll_path).as_bytes())
+        .unwrap();
+    file.write_all(format!("Result: {}\r", format!("{:?}", result)).as_bytes())
+        .unwrap();
+
+    // Closes the tauri application
+    std::process::exit(0);
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,42 +1,15 @@
 #![cfg_attr(
-all(not(debug_assertions), target_os = "windows"),
-windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use std::env;
-use dll_syringe::process::OwnedProcess;
-use dll_syringe::Syringe;
-
-/// Injects a dll into a process
-/// # Arguments
-/// * `process` - The name of the process to inject into
-/// * `dll_path` - The path to the dll to inject
-#[tauri::command]
-fn inject(process: &str, dll_path: &str) {
-    // TODO : Make this not depend on dll_syringe
-
-
-    // Find the process that the user wants to inject into
-    let target_process = OwnedProcess::find_first_by_name(&process).unwrap();
-
-    // Creates Syringe instance with the target process
-    // Injects the dll into the process
-    let instance = Syringe::for_process(target_process);
-
-    let result = instance
-        .inject(dll_path)
-        .unwrap();
-
-    std::fs::write(env::current_dir().unwrap().join("\\sola\\out"), format!("{:?}", result)).unwrap();
-
-    // Closes the tauri application
-    std::process::exit(0);
-}
+mod commands;
+use commands::inject_command;
 
 fn main() {
     // Tauri app builder and run
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![inject])
+        .invoke_handler(tauri::generate_handler![inject_command::run_command])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
